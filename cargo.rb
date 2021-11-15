611c621d421db2702bc112c4dc18e5a3b204c037
gkansala13 <kansalag@oregonstate.edu>
2021-11-14 16:36:17 -0800

    reflect the principles from POODR

---
+++ cargo.rb
@@ -0,0 +1,28 @@
+class Cargo
+
+  MAX_CARGO_ITEMS = 10
+
+  attr_accessor :cargo_contents
+
+  def initialize(bike)
+    @bike = bike
+    @cargo_contents = []
+  end
+
+  def add_cargo(item)
+    self.cargo_contents << item
+  end
+
+  def remove_cargo(item)
+    self.cargo_contents.remove(item)
+  end
+
+  def capacity
+    MAX_CARGO_ITEMS
+  end
+
+  def remaining_capacity
+    MAX_CARGO_ITEMS - self.cargo_contents.size
+  end
+  
+end
