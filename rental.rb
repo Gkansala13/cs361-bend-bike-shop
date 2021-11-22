f55c15f64e11f28c713d0eb342f6104fcc18f612
gkansala13 <kansalag@oregonstate.edu>
2021-11-21 23:04:27 -0800

    reflect the principles from POODR, chapter 3

--- rental.rb
+++ rental.rb
@@ -6,12 +6,25 @@ class Rental
     @bike = bike
   end
 
-  def price
-    self.bike.price + self.bike.luggage.items.count * 10
+  def total_price
+    bike_price + luggage_weight * 2 + self.bike.weight * 2
   end
 
-  def weight
+  def bike_price
+    self.bike.price
+  end
+
+  def luggage_weight
+    self.bike.luggage.items.count * 10
+  end
+
+  def total_weight
     self.bike.weight + self.bike.luggage.items.count
   end
+  # ^ decided not to use this method in the total price equation
+  # This allows me to individually change the both the price multiplyer for the weight and luggage in the future
+
 
 end
+
+
